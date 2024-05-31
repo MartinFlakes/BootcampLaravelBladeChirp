7227ee69bb508cfa7fb65c247e05b11b48173ce9
mlopez <martin.rayer19@gmail.com>
2024-05-31 11:28:09 -0600

    *Showing Chirps

--- chirper/app/Http/Controllers/ChirpController.php
+++ chirper/app/Http/Controllers/ChirpController.php
@@ -15,7 +15,10 @@ class ChirpController extends Controller
      */
     public function index() : View
     {
-        return view('chirps.index');
+        return view('chirps.index', [
+            'chirps' => Chirp::with('user')->latest()->get(),
+            //Agregar paginacion
+        ]);
     }
 
     /**
